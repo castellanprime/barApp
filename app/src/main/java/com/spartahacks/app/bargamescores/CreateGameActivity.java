4a68e0f0d09ddc81c2c8bb70aa5cab3abbd40fb4
castellanprime <okusanya.damilola@gmail.com>
2017-01-21 00:42:16 -0600

    Created scaffolding for BarGameScores App

---
+++ app/src/main/java/com/spartahacks/app/bargamescores/CreateGameActivity.java
@@ -0,0 +1,13 @@
+package com.spartahacks.app.bargamescores;
+
+/**
+ * Created by rogal-dorn on 1/20/17.
+ */
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+
+public class CreateGameActivity extends AppCompatActivity {
+
+}
