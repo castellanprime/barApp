981c2084a72ac2c37cad7dcbccccda54a6533de6
Jared Rickert <jaredrickert52@gmail.com>
2017-01-22 00:01:40 -0600

    added posting to api (#2)

--- app/src/main/java/com/spartahacks/app/bargamescores/JoinGameActivity.java
+++ app/src/main/java/com/spartahacks/app/bargamescores/JoinGameActivity.java
@@ -6,8 +6,25 @@ package com.spartahacks.app.bargamescores;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
+import android.util.Log;
 
 public class JoinGameActivity extends AppCompatActivity {
 
+    private static final String TAG = JoinGameActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        // super.onCreate(savedInstanceState);
+        final Button startButton = (Button) findViewById(R.id.joinactivity_submitBtn);
+        startButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Log.d(TAG, "button pressed");
+                // setContentView(R.layout.activity_joingame);
+            }
+        });
+    }
 }
